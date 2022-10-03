8a7524b8997148e6f505ab00fbea96ad7e7bfd9d
clion <clion@1316c22d-e87f-b044-9b9b-93d7a3e3ba9c>
2022-10-03 11:14:07 +0000

    added cross-platform test
    git-svn-id: https://mysvn.ru/nfrmtk/lykov_cw_new@37 1316c22d-e87f-b044-9b9b-93d7a3e3ba9c

---
+++ tests/is_executable_test.cpp
@@ -0,0 +1,12 @@
+//
+// Created by nfrmtk on 02.10.22.
+//
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include <doctest/doctest/doctest.h>
+#include <lib_test.h>
+
+TEST_CASE("main") {
+
+    CHECK(path_helper().if_executable(std::filesystem::current_path()));
+}
